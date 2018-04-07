f4611a70f69f06d2086c76716bb8542f95aa2d12
Ubuntu <ubuntu@ip-172-31-23-81.us-east-2.compute.internal>
2018-04-07 18:26:32 +0000

    Product add/list tempate code

---
+++ product/js/additem.js
@@ -0,0 +1,38 @@
+// Wait for the DOM to be ready
+$(function() {
+  $("form[name='additem']").validate({
+    rules: {
+      SKU: "required",
+      ItemNo: "required",
+	  Description: "required",
+	  Price: "required",
+	  Manufacturer: "required"
+    },
+    messages: {
+      SKU: "Please enter SKU",
+      ItemNo: "Please enter Item Number",
+	  Description: "Please enter Description",
+	  Price: "Please enter Price",
+	  Manufacturer: "Please enter Manufacturer name"
+    },
+
+    submitHandler: function(form) {
+		$.ajax({
+			url: 'test_post.php', // url where to submit the request
+			type : "POST",
+			dataType : 'json',
+			data : $("#frmadditem").serialize(), // post data || get data
+			success : function(result) {
+				$("#frmMsg").html("Data has been saved successfully.");
+				$( "#frmMsg" ).removeClass( "error").addClass("success");
+				//console.log(result);
+			},
+			error: function(xhr, resp, text) {
+				$("#frmMsg").html("There is some error while procesing your request.");
+				$( "#frmMsg" ).removeClass( "success").addClass("error");
+				//console.log(xhr, resp, text);
+			}
+		})
+    }
+  });  
+});
---
+++ product/test_post.php
@@ -0,0 +1,21 @@
+<?php
+
+
+echo json_encode($_POST);
+
+/*
+{
+  "SKU": "asds",
+  "ItemNo": "adsad",
+  "Description": "asdasd",
+  "Color": "White",
+  "Size": "Large",
+  "Price": "asdas",
+  "Manufacturer": "asdasd",
+  "MfgDate": "",
+  "MfgLocation": "",
+  "ExpiryDate": "",
+  "CurrentLocation": "",
+  "MadeOf": ""
+}
+*/
