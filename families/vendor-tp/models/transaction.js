affb1c75f6a56cecd808ab8d2d7932596e79f4b2
Gaurang Vyas <gdvyas@gmail.com>
2018-05-02 14:01:19 +0530

    Updated tx model.

--- families/vendor-tp/models/transaction.js
+++ families/vendor-tp/models/transaction.js
@@ -1,54 +1,51 @@
 // Definition of Transaction model.
 
 function Transaction(attrs = {}) {
-  this.attrs = attrs;
+	this.attrs = attrs;
 }
 
 Transaction.prototype = {
-  errors: [],
-  isValid(isAppointMent) {
-    const attrs = this.attrs;
-    this.errors = [];
-    if (!attrs.action) {
-      this.errors.push('Action is required');
-    }
+	errors: [],
+	isValid(isAppointMent) {
+		const attrs = this.attrs;
+		this.errors = [];
+		if (!attrs.action) {
+			this.errors.push("Action is required");
+		}
 
-    if (!attrs.asset) {
-      this.errors.push('Asset is required');
-    }
-    else {
-        let assetjson = JSON.parse(asset);
-        if (!assetjson.SKU)
-        {
-            this.errors.push('SKU is required in asset');
-        }
+		if (!attrs.asset) {
+			this.errors.push("Asset is required");
+		} else {
+			if (attrs.isMessage && attrs.isMessage !== "vendorUser") {
+				let assetjson = JSON.parse(attrs.asset);
+				if (!assetjson.SKU) {
+					this.errors.push("SKU is required in asset");
+				}
 
-        if (!assetjson.Manufacturer)
-        {
-            this.errors.push('Manufacturer is required in asset');
-        }
-    }
+				if (!assetjson.Manufacturer) {
+					this.errors.push("Manufacturer is required in asset");
+				}
+			}
+		}
 
-    if (!attrs.owner) {
-      this.errors.push('Owner is required');
-    }
+		if (!attrs.owner) {
+			this.errors.push("Owner is required");
+		}
 
-    if (!attrs.isMessage) {
-      this.errors.push('Message is required');
-    }
+		if (!attrs.isMessage) {
+			this.errors.push("Message is required");
+		}
 
-    if (!attrs.time) {
-      this.errors.push('Time is required');
-    }
+		if (!attrs.time) {
+			this.errors.push("Time is required");
+		}
 
-    if (      !(        attrs.action === 'create'       )    ) {
-      this.errors.push(
-        'Action must be "create" '
-      );
-    }
+		if (!(attrs.action === "create")) {
+			this.errors.push('Action must be "create" ');
+		}
 
-    return this.errors.length === 0;
-  }
+		return this.errors.length === 0;
+	}
 };
 
 module.exports = Transaction;
